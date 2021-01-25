b74e0437dea2a144544c9c64a1bfa0a338f8b87e
The Wondersmith <mark@pawn-pay.com>
2021-01-25 16:00:46 -0500

    Updated to work with Intersolv driver instead of Microsoft

--- run_tests.py
+++
@@ -1,1 +0,0 @@
-from sqlalchemy.dialects import registryimport pytestregistry.register("paradox", "sqlalchemy_paradox.pyodbc", "ParadoxDialect_pyodbc")registry.register("paradox.pyodbc", "sqlalchemy_paradox.pyodbc", "ParadoxDialect_pyodbc")pytest.register_assert_rewrite("sqlalchemy.testing.assertions")from sqlalchemy.testing.plugin.pytestplugin import *
